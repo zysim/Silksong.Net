a95e6b5b5b2f16cf716f4b44b1aa543a8d527298
zysim <9867871+zysim@users.noreply.github.com>
2021-09-03 13:09:10 +0000

    Write Debug Output to File

--- getVidLink.js
+++ getVidLink.js
@@ -1,4 +1,5 @@
 const { Buffer } = require('buffer')
+const fs = require('fs')
 const https = require('https')
 const process = require('process')
 const querystring = require('querystring')
@@ -13,39 +14,58 @@ if (!API_KEY) {
   return 1
 }
 
-const req = https.request(
-  {
-    hostname: 'youtube.googleapis.com',
-    path: `/youtube/v3/search?${querystring.stringify({
-      part: 'snippet',
-      channelId: 'UC9OmOMZS6rU0_jIdZOxSHxw',
-      maxResults: 1,
-      order: 'date',
-      type: 'video',
-      key: API_KEY,
-    })}`,
-    headers: {
-      Accept: 'application/json',
+fs.open('getVidLinkDebugOutput.txt', (openErr, fd) => {
+  if (error) {
+    fs.close(fd, closeErr => {
+      throw closeErr
+    })
+    throw openErr
+  }
+  
+  callYoutubeApi(fd)
+})
+
+const callYoutubeApi = fd => {
+  const req = https.request(
+    {
+      hostname: 'youtube.googleapis.com',
+      path: `/youtube/v3/search?${querystring.stringify({
+        part: 'snippet',
+        channelId: 'UC9OmOMZS6rU0_jIdZOxSHxw',
+        maxResults: 1,
+        order: 'date',
+        type: 'video',
+        key: API_KEY,
+      })}`,
+      headers: {
+        Accept: 'application/json',
+      },
+      method: 'GET',
     },
-    method: 'GET',
-  },
-  (() => {
-    const dataRaw = []
-    return res => {
-      res.on('data', d => {
-        dataRaw.push(d)
-      })
+    (() => {
+      const dataRaw = []
+      return res => {
+        res.on('data', d => {
+          fs.write(fd, d, fileWriteCb)
+          dataRaw.push(d)
+        })
 
-      res.on('end', () => {
-        const dataFull = JSON.parse(Buffer.concat(dataRaw).toString())
-        console.log(dataFull.items[0].id.videoId)
-      })
-    }
-  })(),
-)
+        res.on('end', () => {
+          const dataFull = JSON.parse(Buffer.concat(dataRaw).toString())
+          fs.write(fd, dataFull, fileWriteCb)
+          console.log(dataFull.items[0].id.videoId)
+        })
+      }
+    })(),
+  )
 
-req.on('error', e =>
-  console.error('\x1b[101m\x1b[1m\x1b[38;2;255;255;255m%s\x1b[0m', e),
-)
+  req.on('error', e =>
+    console.error('\x1b[101m\x1b[1m\x1b[38;2;255;255;255m%s\x1b[0m', e),
+  )
 
-req.end()
+  req.end()
+}
+
+const fileWriteCb = (err, bytesWritten) => {
+  if (err) throw err
+}
